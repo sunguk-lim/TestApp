0d0c231fb52259f5b6490152623e007100448017
sunguk-lim <sunguk.lim91@gmail.com>
2021-03-01 00:45:39 +0900

    fitst commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by sunguk.lim on 2021/03/01.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
